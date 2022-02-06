eff2bac20ff1c3ae03134225e4215213e543eb05
Gksll <g_dogan_12@hotmail.com>
2022-02-06 08:48:29 +0300

    Commit1

---
+++ XML/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.lbl_bildirim = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_xml_dosya_olustur = new System.Windows.Forms.Button();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_xml_kaydet = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TSMIDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMIGuncelle = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.Location = new System.Drawing.Point(898, 59);
+            this.btn_temizle.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(236, 138);
+            this.btn_temizle.TabIndex = 26;
+            this.btn_temizle.Text = "Temizle";
+            this.btn_temizle.UseVisualStyleBackColor = true;
+            this.btn_temizle.Click += new System.EventHandler(this.btn_temizle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 254);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1402, 351);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDown);
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(506, 159);
+            this.txt_soyad.Margin = new System.Windows.Forms.Padding(6);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(342, 26);
+            this.txt_soyad.TabIndex = 25;
+            // 
+            // lbl_bildirim
+            // 
+            this.lbl_bildirim.AutoSize = true;
+            this.lbl_bildirim.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_bildirim.ForeColor = System.Drawing.Color.Red;
+            this.lbl_bildirim.Location = new System.Drawing.Point(24, 9);
+            this.lbl_bildirim.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_bildirim.Name = "lbl_bildirim";
+            this.lbl_bildirim.Size = new System.Drawing.Size(76, 26);
+            this.lbl_bildirim.TabIndex = 17;
+            this.lbl_bildirim.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 165);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Soyadı";
+            // 
+            // btn_xml_dosya_olustur
+            // 
+            this.btn_xml_dosya_olustur.Location = new System.Drawing.Point(24, 59);
+            this.btn_xml_dosya_olustur.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_xml_dosya_olustur.Name = "btn_xml_dosya_olustur";
+            this.btn_xml_dosya_olustur.Size = new System.Drawing.Size(342, 138);
+            this.btn_xml_dosya_olustur.TabIndex = 16;
+            this.btn_xml_dosya_olustur.Text = "XML Dosya Oku";
+            this.btn_xml_dosya_olustur.UseVisualStyleBackColor = true;
+            this.btn_xml_dosya_olustur.Click += new System.EventHandler(this.btn_xml_dosya_olustur_Click);
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(506, 109);
+            this.txt_ad.Margin = new System.Windows.Forms.Padding(6);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(342, 26);
+            this.txt_ad.TabIndex = 23;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(506, 59);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(6);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(342, 26);
+            this.txt_id.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 115);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 20);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Adı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "ID";
+            // 
+            // btn_xml_kaydet
+            // 
+            this.btn_xml_kaydet.Location = new System.Drawing.Point(1146, 59);
+            this.btn_xml_kaydet.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_xml_kaydet.Name = "btn_xml_kaydet";
+            this.btn_xml_kaydet.Size = new System.Drawing.Size(236, 138);
+            this.btn_xml_kaydet.TabIndex = 20;
+            this.btn_xml_kaydet.Text = "Kaydet";
+            this.btn_xml_kaydet.UseVisualStyleBackColor = true;
+            this.btn_xml_kaydet.Click += new System.EventHandler(this.btn_xml_kaydet_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMIDelete,
+            this.TSMIGuncelle});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 101);
+            // 
+            // TSMIDelete
+            // 
+            this.TSMIDelete.Name = "TSMIDelete";
+            this.TSMIDelete.Size = new System.Drawing.Size(150, 32);
+            this.TSMIDelete.Text = "Sil";
+            this.TSMIDelete.Click += new System.EventHandler(this.TSMIDelete_Click);
+            // 
+            // TSMIGuncelle
+            // 
+            this.TSMIGuncelle.Name = "TSMIGuncelle";
+            this.TSMIGuncelle.Size = new System.Drawing.Size(240, 32);
+            this.TSMIGuncelle.Text = "Güncelle";
+            this.TSMIGuncelle.Click += new System.EventHandler(this.TSMIGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1402, 605);
+            this.Controls.Add(this.btn_temizle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.lbl_bildirim);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_xml_dosya_olustur);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_xml_kaydet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_temizle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.Label lbl_bildirim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_xml_dosya_olustur;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_xml_kaydet;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TSMIDelete;
+        private System.Windows.Forms.ToolStripMenuItem TSMIGuncelle;
+    }
+}
+
---
+++ XML/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XML
+{
+    public partial class Form1 : Form
+    {
+        XDocument xDoc = new XDocument();
+
+        string directory_path = System.Windows.Forms.Application.StartupPath + "//XML_Islemleri//XML";
+        string file_path = System.Windows.Forms.Application.StartupPath + "//XML_Islemleri//XML//Personeller.xml";
+        int xml_ID = 0;
+        int id;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void DataGridView_Doldur(string path)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(path);
+            try
+            {
+                dataGridView1.DataSource = dataSet.Tables[0];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                lbl_bildirim.Text = "XML Dosyası boş olduğundan gösterilecek veri bulunamadı.";
+            }
+        }
+        void Load_Or_Create_XML()
+        {
+            // eğer bu klasör yoksa klasörü açacak
+            if (Directory.Exists(directory_path) == false)
+            {
+                Directory.CreateDirectory(directory_path);
+            }
+
+            // eğer bu xml dosya yoksa temel bir xml dosyası oluşturacağız.
+            if (File.Exists(file_path) == false)
+            {
+                // verdiğimiz xml dosya yolunda xml dosya oluşturuluyor.
+                FileStream fs = new FileStream(file_path, FileMode.OpenOrCreate);
+
+                // temel bir xml dosyası hazırlıyoruz.
+                string YazilacakIcerik = "<?xml version=\"1.0\" encoding=\"utf-8\" ?> "
+                    + Environment.NewLine + "<Personeller> "
+                    + Environment.NewLine + "</Personeller>";
+                fs.Flush();
+                fs.Close();
+
+                // oluşturulan xml dosyasının okunabilmesi için gerekli olan şeyleri içerisine yazdık.
+                File.AppendAllText(file_path, YazilacakIcerik);
+
+                // xml dosyasını yükledik
+                xDoc = XDocument.Load(file_path);
+                lbl_bildirim.Text = "XML Dosya Oluşturuldu ve Yüklendi.";
+            }
+            else
+            {
+                // zaten bir xml dosyası vardır ve onu yükledik.
+                xDoc = XDocument.Load(file_path);
+                lbl_bildirim.Text = "XML Dosya Yüklendi.";
+            }
+
+            // xml dosyamızdaki verileri datagirdview de  gösterdik.
+            DataGridView_Doldur(file_path);
+        }
+
+        void XML_URLden_Oku()
+        {
+            string URLString = System.Windows.Forms.Application.StartupPath + "\\_023_XML_Islemleri\\XML\\Personeller.xml";
+            XmlTextReader reader = new XmlTextReader(URLString);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:                        
+                        Console.Write("<" + reader.Name);
+                        while (reader.MoveToNextAttribute())                            
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                        Console.Write(">"); Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text:                       
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:                  
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+
+            xDoc = XDocument.Load(URLString);
+            DataGridView_Doldur(URLString);
+
+
+        }
+        private void btn_xml_dosya_olustur_Click(object sender, EventArgs e)
+        {
+            OpenDialog();
+        }
+
+        private void OpenDialog()
+        {
+            OpenFileDialog fileOpen = new OpenFileDialog();
+            fileOpen.Title = "Open XML file";
+            fileOpen.Filter = "XML Files (*.xml)| *.xml";
+
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                file_path = fileOpen.FileName;
+                DataGridView_Doldur(file_path);
+            }
+            fileOpen.Dispose();
+        }
+
+        void XML_Veri_Ekle()
+        {
+            // XML dosyamızı yükledik
+            xDoc = XDocument.Load(file_path);
+
+            //Oluşturduğumuz Root elementine XML dökümanında ki root elementini seçmesini sağlıyoruz.
+            XElement rootElement = xDoc.Root;
+
+            //Yeni bir element oluşturuyoruz.
+            XElement newElement = new XElement("Personel");
+
+            //Elementimize attribute yani Id ataması yapıyoruz.
+            XAttribute idAttribute = new XAttribute("id", txt_id.Text); // <Personel id="1">
+
+            // verilerimiz
+            XElement adiElement = new XElement("ad", txt_ad.Text);  // <ad>Cenk</ad>
+            XElement soyadiElement = new XElement("soyad", txt_soyad.Text); // <soyad>Yiğit</soyad>
+            // verilerimiz
+
+            //Verilerimizi Personel elementimizin içine sıralı bir şekilde ekliyoruz.
+            newElement.Add(idAttribute, adiElement, soyadiElement);
+
+            //Oluşturduğumuz yeni elementi root tagının içine ekliyoruz.
+            rootElement.Add(newElement);
+
+            //Yaptığımız işlemi kayıt ediyoruz.
+            xDoc.Save(file_path);
+
+            DataGridView_Doldur(file_path);
+        }
+        void XML_Veri_Guncelle()
+        {
+            // XML dosyamızı yükledik
+            xDoc = XDocument.Load(file_path);
+
+            XElement rootElement = xDoc.Root;
+            //Oluşturduğumuz Root elementine XML dökümanında ki root elementini seçmesini sağlıyoruz.
+
+            foreach (XElement Personeller in rootElement.Elements())
+            //Root Tagımız içindeki rehber Elementlerinde dönmeye başlıyoruz.
+            {
+                if (Personeller.Attribute("id").Value == id.ToString())
+                //attributes id si 1 olan veri yakalıyoruz Xml Dosyamızda Fatih Olan kişi.
+                {
+                    Personeller.Element("ad").Value = txt_ad.Text;
+                    Personeller.Element("soyad").Value = txt_soyad.Text;
+                    //attributes id si 1 olan anıl adlı kişiyi selim yap.
+                    break;
+                    //Dongüden çık.
+                }
+            }
+            xDoc.Save(file_path);
+            //Yaptığımız işlemi kayıt ediyoruz.
+
+            DataGridView_Doldur(file_path);
+        }
+        void XML_Veri_Sil()
+        {
+            // XML dosyamızı yükledik
+            xDoc = XDocument.Load(file_path);
+
+            XElement rootElement = xDoc.Root;
+            //Oluşturduğumuz Root elementine XML dökümanında ki root elementini seçmesini sağlıyoruz.
+
+            foreach (XElement Personeller in rootElement.Elements())
+            //Root Tagımız içindeki rehber Elementlerinde dönmeye başlıyoruz...
+            {
+                if (Personeller.Attribute("id").Value == id.ToString())
+                //attributes id si 2 olan elementi yakalıyoruz Xml Dosyamızda Seyfi Olan kişi...
+                {
+                    Personeller.Remove();
+                    //attributes id si 2 olan elementi Remove metodu ile siliyoruz.
+                }
+            }
+            xDoc.Save(file_path);
+            //Yaptığımız işlemi kayıt ediyoruz.
+
+            DataGridView_Doldur(file_path);
+        }
+        void Temizle()
+        {
+            xml_ID = 0;
+            txt_id.Text = "";
+            txt_ad.Text = "";
+            txt_soyad.Text = "";
+        }
+
+       
+
+        private void btn_temizle_Click(object sender, EventArgs e)
+        {
+            Temizle();
+        }
+
+        private void btn_xml_kaydet_Click(object sender, EventArgs e)
+        {
+
+
+            XML_Veri_Ekle();
+            Temizle();
+        }
+        private void dataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var hit = dataGridView1.HitTest(e.X, e.Y);
+                dataGridView1.ClearSelection();
+                dataGridView1.Rows[hit.RowIndex].Selected = true;
+                contextMenuStrip1.Show(dataGridView1, e.X, e.Y);
+                id = Convert.ToInt32(dataGridView1.Rows[hit.RowIndex].Cells[2].Value);
+            }
+        }
+
+        private void TSMIDelete_Click(object sender, EventArgs e)
+        {
+            XML_Veri_Sil();
+            Temizle();
+        }
+
+        private void TSMIGuncelle_Click(object sender, EventArgs e)
+        {
+            XML_Veri_Guncelle();
+            Temizle();
+        }
+    }
+}
